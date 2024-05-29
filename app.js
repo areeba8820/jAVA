58a4895422f66799bcadfd7a734252b0af62a861
areeba8820 <162459943+areeba8820@users.noreply.github.com>
2024-05-29 15:42:28 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,43 @@
+let futureStds = []
+console.log(futureStds);
+
+
+let vegNames = ["potato","lemon","tomato","onion"]
+console.log(vegNames);
+
+
+let num = [12,34,69,90,11]
+console.log(num);
+
+
+let booleanArray = [true,false,true,false]
+console.log(booleanArray);
+
+
+let mixedArray = ["pakistan",23,"mango","spiderman",89,true]
+console.log("mango","pakistan");
+
+
+// const stdName = ["Michael","John","Tony"]
+// const stdScore = [320,230,480]
+// let total = 500
+// const percentage1 = (stdScore[0] / total) * 100
+// const percentage2 = (stdScore[1] / total) * 100
+// const percentage3 = (stdScore[2] / total) * 100
+// console.log("score of " + stdName[0] + " is " + stdScore[0] + " . Percentage: " + percentage1 + "%");
+// console.log("<br> score of " + stdName[1] + " is " + stdScore[1] + " . Percentage: " + percentage2 + "%");
+// console.log("<br> score of " + stdName[2] + " is " + stdScore[2] + " . Percentage: " + percentage3 + "%");
+
+
+const stdName = ["Michael","John","Tony"]
+const stdScore = [320,230,480]
+let total = 500
+const percentage1 = (stdScore[0] / total) * 100
+const percentage2 = (stdScore[1] / total) * 100
+const percentage3 = (stdScore[2] / total) * 100
+document.write("score of " + stdName[0] + " is " + stdScore[0] + " . Percentage: " + percentage1 + "%");
+document.write("<br> score of " + stdName[1] + " is " + stdScore[1] + " . Percentage: " + percentage2 + "%");
+document.write("<br> score of " + stdName[2] + " is " + stdScore[2] + " . Percentage: " + percentage3 + "%");
+
+
+
